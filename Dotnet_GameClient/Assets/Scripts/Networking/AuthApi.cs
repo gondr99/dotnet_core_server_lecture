d4f5b8ac7aa145faab4f6ef5caf2c3bed8d34889
gondr99 <gondr99@gmail.com>
2026-09-12 14:53:00 +0900

    lecture2 middle

---
+++ Dotnet_GameClient/Assets/Scripts/Networking/AuthApi.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Networking.Dtos;
+
+namespace Networking
+{
+    public static class AuthApi
+    {
+        public static Task<ApiResult<UserResponse>> RegisterAsync(string username, string nickname, string password,
+            CancellationToken ct = default)
+        {
+            RegisterRequest reqBody = new RegisterRequest
+            {
+                Username = username,
+                Nickname = nickname,
+                Password = password
+            };
+            
+            return ApiClient.Instance.PostAsync<UserResponse>("/api/auth/register", reqBody, ct);
+        }
+    }
+}
---
+++ Dotnet_GameClient/Assets/Scripts/Networking/Dtos/AuthDtos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Networking.Dtos
+{
+    public class RegisterRequest
+    {
+        public string Username { get; set; }
+        public string Nickname { get; set; }
+        public string Password { get; set; }
+    }
+    
+    public class UserResponse
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string Nickname { get; set; } = null!;
+        public long Gold { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
